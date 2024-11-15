b228c2f2a1663e1219a78716c39fac69dc23c7e1
yun9699 <cha23215@daum.net>
2024-11-15 15:38:54 +0900

    feat: FAQ LIST PAGE ADD

--- src/api/csAPI/faqAPI.ts
+++ src/api/csAPI/faqAPI.ts
@@ -4,7 +4,7 @@ const host = `http://localhost:8080/api/v1/faq`;
 
 // FAQ 목록 조회
 export const getFAQList = async (page: number = 1, size: number = 10, fcategory?: string) => {
-    const res = await axios.get(`${host}/list?page=${page}&size=${size}${fcategory ? `&fcategory=${fcategory}` : ''}`);
+    const res = await axios.get(`${host}/list/${fcategory}?page=${page}&size=${size}`);
     console.log(res.data);
     return res.data;
 };
