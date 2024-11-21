a8609cdc7c4c9faac9392b3f1038f566a59ce186
DSL0680 <ekthf988172@gmail.com>
2024-11-21 09:20:48 +0900

    feat : pinia edit / camera warning add

---
+++ src/api/UserAPI/userAllergyAPI.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const compareUserAllergies = async (uno: number, imageAllergies: string[]): Promise<string[]> => {
+    const response = await axios.post(`http://localhost:8080/api/v1/user/compare-allergies/${uno}`, imageAllergies);
+    return response.data;
+};
--- src/stores/useUser.ts
+++ src/stores/useUser.ts
@@ -1,23 +1,22 @@
-import {defineStore} from "pinia";
-import {computed, ref} from "vue";
+// userStore.ts
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
 const useUser = defineStore('userStore', () => {
-
-    const user = ref({uno: 0});
-
+    const user = ref<{ uno?: number }>({});
     const personalAllergies = ref<{ anos: number[] }>({ anos: [] });
 
     const setUno = (uno: number) => user.value.uno = uno;
-
     const setPersonalAllergies = (anos: number[]) => {
         personalAllergies.value.anos = anos;
     };
 
-    const getUno =  computed(() => user.value.uno);
-
+    const getUno = computed(() => user.value.uno);
     const getPersonalAllergies = computed(() => personalAllergies.value.anos);
 
-    return {user, setUno, getUno, personalAllergies, setPersonalAllergies, getPersonalAllergies};
+    return { user, setUno, getUno, personalAllergies, setPersonalAllergies, getPersonalAllergies };
+}, {
+    persist: true
 });
 
-export default useUser
+export default useUser;
