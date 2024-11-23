5e2947c37679f136f70926a77e4f5209e8dd0e4b
jhyeong <hyeongcode@gmail.com>
2024-11-23 17:42:42 +0900

    clore: cart list

--- src/components/cartcomponent/CartListComponent.vue
+++ src/components/cartcomponent/CartListComponent.vue
@@ -1,43 +1,75 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import {getCartList} from "../../api/cartapi/cartapi.ts";
-import {IPageRequest, IPageResponse} from "../../types/commonTypes.ts";
-import {ICartItem} from "../../types/cartTypes.ts";
+import {onBeforeUnmount, onMounted, ref} from 'vue';
+import { getCartList } from "../../api/cartapi/cartapi.ts";
+import { ICartItem } from "../../types/cartTypes.ts";
+import { InitPageResponse } from "../../init/CommonInit.ts";
+import { IPageResponse } from '../../types/commonTypes';
 
+const initCartItem: ICartItem = {
+  price: 0,
+  pno: 0,
+  ptitle_ko: '',
+  ptitle_en: '',
+  ptitle_zh: '',
+  ptitle_ja: '',
+  pfilename: '',
+  ciqty: 0
+};
 
-const initPageRequest: IPageRequest = {
+const initPage = InitPageResponse<ICartItem>();
 
-  page: 0,
-  size: 0,
-  type: '',
-  keyword: '',
-  arr: [],
-  skip: 0
-}
+const data = ref<{ pageData: IPageResponse<ICartItem> }>({
 
-const initPageResponse: IPageResponse<ICartItem> = {
-
-  list: <ICartItem>[],
-  total: 0,
-  startPage: 0,
-  endPage: 0,
-  prev: false,
-  next: false,
-  pageRequest: initPageRequest
-}
-
-const cartList = ref<{ cartListData: ICartItem[] }>({
-
-  cartListData: initPageResponse.list,
+  pageData: initPage
 });
 
+const tmpData = ref<ICartItem[]>([
+  {...initCartItem}
+])
+
+const deleteItem = (pno: number) => {
+
+  tmpData.value = tmpData.value.filter((item) => item.pno !== pno);
+}
+
+const increaseQty = (pno: number) => {
+
+  const item = tmpData.value.find((item) => item.pno === pno)
+
+  if (item) {
+    item.ciqty++;
+  }
+}
+
+const decreaseQty = (pno: number) => {
+
+  const item = tmpData.value.find((item) => item.pno === pno)
+
+  if (item && item.ciqty > 1) {
+    item.ciqty--;
+  }
+}
+
 onMounted(() => {
+  console.log(data.value.pageData.list);
 
   getCartList().then((res) => {
 
-    cartList.value = res;
+    console.log(res);
+
+    data.value.pageData.list = res;
+    tmpData.value = res.list;
   })
+});
+
+onBeforeUnmount(() => {
+
+  if(data.value.pageData.list != tmpData.value) {
+
+    console.log("aaaaaaaa");
+  }
 })
+
 </script>
 
 <template>
@@ -45,35 +77,45 @@ onMounted(() => {
     <h1 class="text-2xl font-bold mb-4">장바구니</h1>
     <ul class="bg-white rounded-lg shadow-lg divide-y divide-gray-200">
       <li
-          v-for="item in cartList"
-          :key="item.id"
+          v-for="item in tmpData"
+          :key="item.pno"
           class="flex items-center justify-between p-4 hover:bg-gray-50"
       >
         <!-- 상품 이름 -->
         <span class="font-medium text-gray-800">{{ item.ptitle_ko }}</span>
         <!-- 상품 수량 및 조정 -->
         <div class="flex items-center space-x-2">
+
           <!-- 감소 버튼 -->
           <button
-              @click="decreaseQuantity(item.id)"
+              @click="decreaseQty(item.pno)"
               class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-1 px-2 rounded"
           >
             -
           </button>
+
           <!-- 상품 수량 -->
-          <span class="text-gray-800 font-medium">{{ item.cqty }}</span>
+          <span class="text-gray-800 font-medium">{{ item.ciqty }}</span>
+
           <!-- 증가 버튼 -->
           <button
-              @click="increaseQuantity(item.id)"
+              @click="increaseQty(item.pno)"
               class="bg-gray-200 hover:bg-gray-300 text-gray-600 font-bold py-1 px-2 rounded"
           >
             +
+          </button>
+
+          <!-- 삭제 버튼 -->
+          <button
+              @click="deleteItem(item.pno)"
+              class="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
+          >
+            삭제
           </button>
         </div>
       </li>
     </ul>
   </div>
-
 </template>
 
 <style>
