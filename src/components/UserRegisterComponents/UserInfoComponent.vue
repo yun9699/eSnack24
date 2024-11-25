dd1808a2136bbb78298fd18b6507fc3da2ef5383
yun9699 <cha23215@daum.net>
2024-11-25 13:16:36 +0900

    feat: User Address info input locale

--- src/components/UserRegisterComponents/UserInfoComponent.vue
+++ src/components/UserRegisterComponents/UserInfoComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { inject, ref } from "vue"
+import { inject } from "vue"
 import { useI18n } from 'vue-i18n'
 import { IUserInfo } from "../../types/userRegisterTypes.ts"
 
