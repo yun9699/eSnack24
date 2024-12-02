c6110f56d9732b488f9762d451691e9747493766
DSL0680 <ekthf988172@gmail.com>
2024-12-02 16:19:40 +0900

    chore: url https edit

--- src/api/AllergyAPI/allergyAPI.ts
+++ src/api/AllergyAPI/allergyAPI.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const host = 'https://3.34.191.185:8080/api/v1/allergy'
+const host = 'https://esnack24.store:8080/api/v1/allergy'
 
 export const getAllergyListKo = async () => {
 
