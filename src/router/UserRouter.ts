9d161b5c693b234c3497e10c7baf39fffa3baecb
eoneon <duqdjs123@naver.com>
2024-12-05 17:02:19 +0900

    merge: merge

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -1,7 +1,6 @@
 
 
 const UserIndexPage = () => import('../pages/userpages/UserIndexPage.vue')
-const UserEditPage = () => import("../pages/userpages/UserEditPage.vue")
 const UserMainPage = () =>  import('../pages/userpages/UserMainPage.vue')
 const UserEditPage = () =>  import('../pages/userpages/UserEditPage.vue')
 const UserOrderListPage = () =>  import("../pages/userpages/UserOrderListPage.vue")
