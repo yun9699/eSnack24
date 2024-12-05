6119eec734f68f96f647e510c3472f34296c380b
eoneon <duqdjs123@naver.com>
2024-12-05 17:00:07 +0900

    feat: button togle

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -1,5 +1,7 @@
 
+
 const UserIndexPage = () => import('../pages/userpages/UserIndexPage.vue')
+const UserEditPage = () => import("../pages/userpages/UserEditPage.vue")
 const UserMainPage = () =>  import('../pages/userpages/UserMainPage.vue')
 const UserOrderListPage = () =>  import("../pages/userpages/UserOrderListPage.vue")
 
