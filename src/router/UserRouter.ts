b3cee99d8ba6ce6efcb92b75b7d4186ae53785cc
DSL0680 <ekthf988172@gmail.com>
2024-12-05 17:29:57 +0900

    user edit page add

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -2,6 +2,7 @@
 const UserIndexPage = () => import('../pages/userpages/UserIndexPage.vue')
 const UserMainPage = () =>  import('../pages/userpages/UserMainPage.vue')
 const UserOrderListPage = () =>  import("../pages/userpages/UserOrderListPage.vue")
+const UserEditPage = () =>  import('../pages/userpages/UserEditPage.vue')
 
 
 
