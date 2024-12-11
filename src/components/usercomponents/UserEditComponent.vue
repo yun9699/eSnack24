666fb3680d991576a6cc932e4831d52bc663f10e
eoneon <duqdjs123@naver.com>
2024-12-11 10:40:44 +0900

    chore: userinfo locales edit

--- src/components/usercomponents/UserEditComponent.vue
+++ src/components/usercomponents/UserEditComponent.vue
@@ -157,15 +157,15 @@
             class="w-full flex items-center p-4 hover:bg-gray-50"
             @click="isAllergyOpen = !isAllergyOpen"
         >
-          <div class="flex items-center flex-1">
-            <div class="w-10 h-10 flex items-center justify-center bg-gray-100 rounded-full mr-3">
+          <div class="flex items-center flex-1 relative">
+            <div class="absolute left-0 w-10 h-10 flex items-center justify-center bg-gray-100 rounded-full mr-3">
               <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </div>
-            <div>
-              <h3 class="text-base font-medium text-gray-900">알레르기 변경</h3>
-              <p class="text-sm text-gray-500">본인의 알레르기 정보를 변경해주세요.</p>
+            <div class="pl-14">
+              <h3 class="text-base font-medium text-gray-900">{{ t('edit_user.allergy_change') }}</h3>
+              <p class="text-sm text-gray-500">{{ t('edit_user.person_allergy_change') }}</p>
             </div>
           </div>
           <svg
